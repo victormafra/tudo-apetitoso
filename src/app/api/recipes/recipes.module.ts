40cf4e96cabe4599ae03006168099c86388d7044
Victor Lucas de Melo Mafra <vlmmafra@gmail.com>
2019-11-06 11:28:39 -0300

    Adicionar primeira versão de layout

--- src/app/api/recipes/recipes.module.ts
+++ src/app/api/recipes/recipes.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
-
-
+import { MatCardModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [RecipesListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatButtonModule
   ],
   exports: [RecipesListComponent]
 })
